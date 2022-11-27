93dc574513909e3df0b3439b3cc95f2f4a304cdd
Madeleinesj <m.marchant-daisley@hotmail.com>
2022-11-27 15:11:53 -0800

    js projects first push

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+//1.// 
+var A = "Hello, Freya!" 
+window.alert(A);
+
+//2.// 
+var B = "Today"
+var C = " is beautiful"
+
+//3.//
+document.write(B+C);
+
+//4.//
+var D = (2+2)
+document.write(D);
+
+//5.//
+//Comments//
+
+//6.//
+//linked to main file//
