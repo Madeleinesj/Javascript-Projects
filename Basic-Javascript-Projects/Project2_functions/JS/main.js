620197c729584d9a76f3f8f97d0a4b47267eddfe
Madeleinesj <m.marchant-daisley@hotmail.com>
2022-11-27 18:12:05 -0800

    project2_function submission

---
+++ Basic-Javascript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "This is the best button!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this class!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
