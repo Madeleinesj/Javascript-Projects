926746dca4d5683466b5df96fcfb41544ce8d343
Madeleinesj <m.marchant-daisley@hotmail.com>
2022-12-03 09:27:19 -0800

    Project7

---
+++ Basic-Javascript-Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,52 @@
+//Global variable will read in function line 3 since it is outside of function//
+var X = 2;
+function add() {
+    document.write(X + 2);
+}
+
+//local variable//
+function sub() {
+    var Y = 3;
+    document.write(Y - 1);
+}
+
+//Found Y is not defined in console.log//
+//had to delete function due to causing error for following function//
+
+
+//If Statements//
+
+if (new Date().getHours() < 18) {
+    document.getElementById("demo").innerHTML = "Good day!";
+}
+
+//Else statements//
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 16) {
+        Vote = "You are old enought to drive!";
+    }
+    else {
+        Vote = "You are not old enough to drive!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+//Else if statements//
+
+function Season_function() {
+    var Month = new Date().getMonth();
+    var Reply;
+    if (Month < 12 == Month > 9) {
+        Reply = "It is Autumn time!";
+    }
+    else if (Month >= 4 == Month < 9) {
+        Reply = "It is Spring/Summer time!";
+    }
+    else {
+        Reply = "It is Winter time!.";
+    }
+    document.getElementById("Time_of_year").innerHTML = Reply;
+}
+
